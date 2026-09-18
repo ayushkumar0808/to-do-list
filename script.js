a20fe70f14e69b5641f3d6bd6ad5b4f42f7fb66c
Ayush Kumar <kayush3647@gmail.com>
2026-09-18 12:18:59 +0530

    add

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 let itemContainer = document.querySelector(".itemContainer");
 let inputTask = document.querySelector("#inputTask");
 let btn = document.querySelector("#btn");
+// let deleteBtn = document.querySelector(".deleteBtn");
+let list = [
+  {
+    id: 123,
+    text: "hello",
+  },
+  {
+    id: 12356,
+    text: "what",
+  },
+];
 
-const handleClick = () => {
-  let div = document.createElement("div");
-  div.className = "item";
-  div.innerHTML = `
-    <p>${inputTask.value}</p>
-      <button class="deleteBtn">❌</button>`;
-  itemContainer.append(div);
+const handleAdd = () => {
+  let obj = {
+    id: Date.now(),
+    text: inputTask.value,
+  };
+  list.unshift(obj);
+  render(list);
 };
+btn.addEventListener("click", handleAdd);
 
-btn.addEventListener("click", handleClick);
-
-inputTask.addEventListener("change", () => {});
+function render(arr) {
+  itemContainer.innerHTML = "";
+  arr.map((obj) => {
+    console.log(obj.text);
+    let div = document.createElement("div");
+    div.className = "item";
+    div.innerHTML = `
+    <p>${obj.text}</p>
+      <button class="deleteBtn">❌</button>`;
+    itemContainer.append(div);
+  });
+}
+render(list);
